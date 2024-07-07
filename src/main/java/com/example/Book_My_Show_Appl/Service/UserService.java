ffcdb27dfb6271058b42da5bc9d57626be033608
Akash <akash.patil4837@gmail.com>
2024-07-07 19:39:44 +0530

    Email sending

--- src/main/java/com/example/Book_My_Show_Appl/Service/UserService.java
+++ src/main/java/com/example/Book_My_Show_Appl/Service/UserService.java
@@ -5,6 +5,8 @@ import com.example.Book_My_Show_Appl.DTO.Request.AddUserRequest;
 import com.example.Book_My_Show_Appl.Model.User;
 import com.example.Book_My_Show_Appl.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +14,9 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    JavaMailSender javaMailSender;
 
     public String addUser(AddUserRequest addUserRequest){
 
@@ -21,7 +26,21 @@ public class UserService {
                 .mobileNo(addUserRequest.getMobileNo())
                 .build();
 
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setTo(addUserRequest.getEmailId());
+        mailMessage.setFrom("rideitis3@gmail.com");
+        mailMessage.setSubject("Welcome to Book My Show!!");
+
+        String body = "Hi " + addUserRequest.getName() + " !" + "Welcome to Book My Show, Use WELCOME10 to get 10% off on tickets";
+
+        mailMessage.setText(body);
+
+        javaMailSender.send(mailMessage);
+
         user = userRepository.save(user);
+
+
 
         return "User has been saved to the DB with userId " + user.getUserId();
     }
